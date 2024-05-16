7e0d9b8c76defd664321ec7c35a25c24725a021b
Rajeev-Kumar-DSA <rajeevkr2061998@gmail.com>
2024-05-17 01:14:58 +0530

    add all method call

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -10,6 +10,7 @@ public class F1 extends Car {
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed = getCurrentSpeed() + rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,10 +21,24 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if(newSpeed <= 0){
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
+        }//for all other cases, change the gear accordingly
+        else if(newSpeed <= 50){
+            changeGear(1);
+        }else if (newSpeed <= 100){
+            changeGear(2);
+        }else if (newSpeed <= 150){
+            changeGear(3);
+        }else if (newSpeed <= 200){
+            changeGear(4);
+        }else if (newSpeed <= 250){
+            changeGear(5);
+        }else{
+            changeGear(6);
         }
-        //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
