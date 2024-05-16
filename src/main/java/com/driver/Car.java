304154c2a9f1b4969cad00b9fdd4fba9a5c4a126
Rajeev-Kumar-DSA <rajeevkr2061998@gmail.com>
2024-05-17 01:21:24 +0530

    add all method call - update the getting attribute

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -21,6 +21,30 @@ public class Car extends Vehicle {
         this.currentGear = 1;
     }
 
+    public int getWheels(){
+        return wheels;
+    }
+
+    public int getDoors(){
+        return doors;
+    }
+
+    public int getGears(){
+        return gears;
+    }
+
+    public boolean isManual(){
+        return isManual;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public int getSeats(){
+        return seats;
+    }
+
     public Car(String name, boolean isManual){
         super(name);
         this.isManual = isManual;
